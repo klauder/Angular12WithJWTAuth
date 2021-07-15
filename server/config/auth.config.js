3a44c457fbae4f1b483758fb1b247f3b4b18820e
Klauder <klauderdias@gmail.com>
2021-07-14 22:42:18 -0300

    Configuração do Back-end. (Não funciona)

---
+++ server/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "angular12-with-JWT-secret-key"
+};
--- server/index.js
+++ server/index.js
@@ -10,6 +10,33 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+// Config DB
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+// Function to create 3 roles
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
@@ -20,6 +47,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Node.js Back-end application with JWT." });
 });
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
